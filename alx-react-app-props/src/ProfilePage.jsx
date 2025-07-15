7eb159e21a27211580f3efa7a547fac209e9f813
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-07-15 12:29:57 +0000

    Remove alx-react-app-props directory to start fresh

--- alx-react-app-props/src/ProfilePage.jsx
+++
@@ -1,8 +0,0 @@
-import UserInfo from './UserInfo';
-
-function ProfilePage() {
-  // Simply render UserInfo; no props needed
-  return <UserInfo />;
-}
-
-export default ProfilePage;
--- alx-react-app-props/src/UserContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-// Create a context with default value null
-const UserContext = React.createContext(null);
-
-export default UserContext;
