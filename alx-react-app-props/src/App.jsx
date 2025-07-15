1fa3236c6b977e9a76f607c559b51561ca8ab75c
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-07-15 13:04:26 +0000

    Refactored to use Context API in alx-react-app-props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,6 @@
 import ProfilePage from './components/ProfilePage';
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
+
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
