8ae066d4bd696ae34e5c13275bb32b0be743b74a
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-07-22 09:43:56 +0000

    Add recipe sharing app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,29 @@
-import AddRecipeForm from './components/AddRecipeForm';
+import { Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <div>
       <h1>🍲 Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <hr />
-      <RecipeList />
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <hr />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 }
