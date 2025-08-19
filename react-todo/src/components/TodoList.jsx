73c9f7d8f4b20285481c17612b20fea89d35325f
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-08-19 12:56:49 +0000

    renamed App.js to App.jsx

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,57 +1,13 @@
-import React, { useState } from "react";
-import AddTodoForm from "./AddTodoForm";
+import React from "react";
+import TodoList from "./components/TodoList";
 
-export default function TodoList() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "Learn React", completed: false },
-    { id: 2, text: "Build a Todo App", completed: true },
-    { id: 3, text: "Write tests", completed: false },
-  ]);
-
-  const addTodo = (text) => {
-    const newTodo = { id: Date.now(), text, completed: false };
-    setTodos([...todos, newTodo]);
-  };
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
+function App() {
   return (
-    <div>
-      <h2>Todo List</h2>
-      <AddTodoForm addTodo={addTodo} />
-      <ul>
-        {todos.map((todo) => (
-          <li
-            key={todo.id}
-            onClick={() => toggleTodo(todo.id)}
-            style={{
-              textDecoration: todo.completed ? "line-through" : "none",
-              cursor: "pointer",
-            }}
-            data-testid="todo-item"
-          >
-            {todo.text}
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                deleteTodo(todo.id);
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+    <div style={{ padding: "20px" }}>
+      <h1>React Todo App</h1>
+      <TodoList />
     </div>
   );
 }
+
+export default App;
