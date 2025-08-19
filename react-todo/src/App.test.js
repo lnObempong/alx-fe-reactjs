a3dece13b119799500fcf70ecb8574f9bccd7847
Leonard Nketia Obempong <lnobempong@gmail.com>
2025-08-19 13:27:36 +0000

    Add these

--- react-todo/src/App.test.js
+++ react-todo/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the main TodoList component in App", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText("Todo List")).toBeInTheDocument();
 });
